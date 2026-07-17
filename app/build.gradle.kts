7d0831f124fd6abb1927c0489788406a59521bd5
sn19900-svg <sn19900@gmail.com>
2026-07-17 22:22:28 +0300

    إصلاح: الانتقال من kotlinOptions المهجور إلى compilerOptions DSL الجديد (Kotlin 2.3.21)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,12 @@ plugins {
     id("com.google.devtools.ksp")
 }
 
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
+}
+
 android {
     namespace = "com.mostawdai"
     compileSdk = 36
@@ -28,10 +34,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
     }
 
     buildFeatures {
